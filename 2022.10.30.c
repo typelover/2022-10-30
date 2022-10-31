fe85879ad70510d861c1842702db426dc0654626
typelover <116749133+typelover@users.noreply.github.com>
2022-10-31 17:25:20 +0800

    Add files via upload

---
+++ 2022.10.30.c
@@ -0,0 +1,23 @@
+#pragma warning(disable:4996)
+# include <stdio.h>
+#include <string.h>
+int  main()
+{
+	char ch[] = "zengningxing";
+	printf("sizeof ch =%d\n", sizeof(ch));
+	printf("strleng ")
+
+	return 0;
+}
+
+
+//int main()
+//{
+//	int a[10] = { [3] = 3,[6] = 6,[8] = 8 };
+//	for (int i = 0; i < 10; i++)
+//	{
+//		printf("%d\n", a[i]);
+//	}
+//	return 0;
+//}
+
